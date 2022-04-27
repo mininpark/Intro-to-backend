1cccdd4f939ef6fe035cccb485a1f6099cf312ea
mininpark <parkmini.93@gmail.com>
2022-04-27 18:12:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+
+var mySuperheroName = superheroes.random();
+var mySupervillainsName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainsName);
